7bc2432a83241637b77b08d8568655adb0e5fe73
jasminecheema19 <jasmine_cheema_95@yahoo.co.in>
2016-06-12 17:57:55 +0530

    Partial completion of Assignment 1

---
+++ assign1.R
@@ -0,0 +1,6 @@
+refine_original <- read.csv("refine_original.csv")
+refine_original <- separate(refine_original, Product.code...number, c("product_code", "product_number"))
+refine_original <- unite(refine_original, full_address, address, city, country, sep=",", remove = FALSE)
+category <- c("Smartphone", "Smartphone", "Laptop", "Laptop", "Laptop", "Smartphone", "TV", "TV", "Laptop", "Smartphone", "Tablet", "Tablet", "Laptop", "Smartphone", "TV", "TV", "Laptop", "TV", "TV", "Laptop", "Smartphone", "Laptop", "Tablet", "Tablet", "Tablet")
+refine_original$category <- category
+
